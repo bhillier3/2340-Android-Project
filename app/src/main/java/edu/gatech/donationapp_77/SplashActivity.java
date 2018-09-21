b34be5a6d48e60ca9261fe4f77dd0a8d42c1f7de
wjacobc <wjacobchambers@gmail.com>
2018-09-20 20:39:52 -0400

    Added SplashActivity that links to LoginActivity.

---
+++ app/src/main/java/edu/gatech/donationapp_77/SplashActivity.java
@@ -0,0 +1,25 @@
+package edu.gatech.donationapp_77;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
