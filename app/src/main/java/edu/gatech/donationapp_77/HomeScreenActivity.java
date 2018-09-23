765f8149f188f3cc4fc16f18c49f465834f52bdc
bhillier3 <benrhillier98@gmail.com>
2018-09-23 01:13:09 -0400

    unfinished home screen with logout functionality

---
+++ app/src/main/java/edu/gatech/donationapp_77/HomeScreenActivity.java
@@ -0,0 +1,29 @@
+package edu.gatech.donationapp_77;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        logout();
+    }
+
+    private void logout() {
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Back to splash screen
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/edu/gatech/donationapp_77/LoginActivity.java
+++ app/src/main/java/edu/gatech/donationapp_77/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.gatech.donationapp_77;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -174,6 +175,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            // Open to the home screen
+            startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
         }
     }
 
